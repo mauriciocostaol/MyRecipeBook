36c19d2fd2febb87c9a2f5b0ef76bef15ae2fb96
Mauricio Ricardo <mauricic28@hotmail.com>
2024-05-22 13:59:02 -0300

    Adding Unit Tests and Integration Tests for UseCase,Validators ans API

---
+++ tests/ComonTestUtilities/Repositories/UserWriteOnlyRepositoryBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Moq;
+using MyRecipeBook.Domain.Repositories.User;
+
+namespace ComonTestUtilities.Repositories;
+public class UserWriteOnlyRepositoryBuilder
+{
+    public static IUserWriteOnlyRepository Build()
+    {
+        var mock = new Mock<IUserWriteOnlyRepository>();
+
+        return mock.Object;
+    }
+}
---
+++ tests/UseCases.Test/User/Register/RegisterUserUseCaseTest.cs
@@ -0,0 +1,74 @@
+﻿using ComonTestUtilities.Cryptography;
+using ComonTestUtilities.Mapper;
+using ComonTestUtilities.Repositories;
+using ComonTestUtilities.Requests;
+using FluentAssertions;
+using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionsBase;
+
+namespace UseCases.Test.User.Register;
+public class RegisterUserUseCaseTest
+{
+    [Fact]
+    public async Task SuccessOnRegisterUserWhenRequesiIsValid()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+        var useCase = CreateUseCase();
+
+        var result = await useCase.Execute(request);
+
+        result.Should().NotBeNull();
+        result.Name.Should().Be(request.Name);
+    }
+
+    [Fact]
+    public async Task ErrorOnRegisterWhenEmailAlreadyRegistered()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+        
+        var useCase = CreateUseCase(request.Email);
+
+        Func<Task> act = async () => await useCase.Execute(request);
+
+        (await act.Should().ThrowAsync<ErrorOnValidationException>())
+             .Where(error => error.ErrorMessages.Count == 1 && error.ErrorMessages.Contains(ResourceMessagesExceptions.EMAIL_ALREADY_REGISTERED));
+       
+       
+
+    }
+
+
+    [Fact]
+    public async Task ErrorOnRegisterWhenNameIsEmpty()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Name = string.Empty;
+        var useCase = CreateUseCase();
+
+        Func<Task> act = async () => await useCase.Execute(request);
+
+        (await act.Should().ThrowAsync<ErrorOnValidationException>())
+             .Where(error => error.ErrorMessages.Count == 1 && error.ErrorMessages.Contains(ResourceMessagesExceptions.NAME_EMPTY));
+
+
+
+    }
+
+
+    private  RegisterUserUseCase CreateUseCase(string? email = null)
+    {
+
+        var mapper = MapperBuilder.Build();
+        var passwordEncripter = PasswordEncripterBuilder.Build();
+        var writeOnlyRepostory = UserWriteOnlyRepositoryBuilder.Build();
+        var unitOfWork = UnitOfWorkBuilder.Build();
+        var readOnlyRepostoryBuilder = new UserReadOnlyRepositoryBuilder();
+
+        if(!string.IsNullOrEmpty(email))
+            readOnlyRepostoryBuilder.ExistActiveUserWithEmail(email);
+
+
+        return new RegisterUserUseCase(mapper, writeOnlyRepostory, readOnlyRepostoryBuilder.Build(), passwordEncripter, unitOfWork);
+    }
+}
