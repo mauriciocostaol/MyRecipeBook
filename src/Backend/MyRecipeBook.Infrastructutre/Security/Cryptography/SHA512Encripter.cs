9bab386927e238510b4daa363e57a435139299bf
Mauricio Ricardo <mauricic28@hotmail.com>
2024-07-31 22:26:34 -0300

    Adding functions to use database

---
+++ src/Backend/MyRecipeBook.Infrastructutre/Security/Cryptography/SHA512Encripter.cs
@@ -0,0 +1,30 @@
+﻿using MyRecipeBook.Domain.Security.Cryptography;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MyRecipeBook.Infrastructutre.Security.Cryptography;
+public class SHA512Encripter: IPasswordEncripter
+{
+    private readonly string _additionalKey;
+    public SHA512Encripter(string additionalKey) => _additionalKey = additionalKey;
+
+    public string Encrypt(string password)
+    {
+
+        var newPassword = $"{password}{_additionalKey}";
+        var bytes = Encoding.UTF8.GetBytes(newPassword);
+        var hashBytes = SHA512.HashData(bytes);
+        return StringBytes(hashBytes);
+    }
+
+    private static string StringBytes(byte[] bytes)
+    {
+        var sb = new StringBuilder();
+        foreach (byte b in bytes)
+        {
+            var hex = b.ToString("x2");
+            sb.Append(hex);
+        }
+        return sb.ToString();
+    }
+}
---
+++ src/Backend/MyRecipeBook.Infrastructutre/Services/LoggedUser/LoggedUser.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyRecipeBook.Domain.Entities;
+using MyRecipeBook.Domain.Security.Tokens;
+using MyRecipeBook.Domain.Services.LoggedUser;
+using MyRecipeBook.Infrastructutre.DataAccess;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace MyRecipeBook.Infrastructutre.Services.LoggedUser;
+public class LoggedUser : ILoggedUser
+{
+    private readonly MyRecipeBookDbContext _dbContext;
+    private readonly ITokenProvider _tokenProvider;
+
+    public LoggedUser(MyRecipeBookDbContext dbContext, ITokenProvider tokenProvider)
+    {
+
+        _dbContext = dbContext;
+        _tokenProvider = tokenProvider;
+
+    }
+    public async Task<User> User()
+    {
+        var token = _tokenProvider.Value();
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var jwtSecurityToken = tokenHandler.ReadJwtToken(token);
+
+        var identifier = jwtSecurityToken.Claims.First(c => c.Type == ClaimTypes.Sid).Value;
+        var userIdentifier = Guid.Parse(identifier);
+        return await _dbContext
+            .Users
+            .AsNoTracking()
+            .FirstAsync(user => user.Active && user.UserIdentifier == userIdentifier );
+    }
+}
